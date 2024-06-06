e78600da3b6b01a6b5053d014de53c1d7182fe2d
ThiagoMatumotoYouse <thiago.matumoto@youse.com.br>
2024-06-06 19:43:39 -0300

    feat: :sparkles: add footer
    add footer

--- src/ui/Components/Footer.jsx
+++ src/ui/Components/Footer.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import SeafetyLogo from "../../Assets/Seafety.svg";
+import { useNavigate } from "react-router-dom";
 
-const Footer = props => {
+const Footer = (props) => {
+  const navigate = useNavigate();
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <img
+        onClick={() => navigate("/")}
+        src={SeafetyLogo}
+        alt=""
+        srcset=""
+        className="footer-img-seafety"
+      />
+      <section className="footer-info">
+        <p>@2024 Seafety Ocean</p>
+        <p>by Ana, Clara, Lúcia, Thiago e Miguel </p>
+        <p>FIAP</p>
+      </section>
+    </footer>
+  );
+};
 
-Footer.propTypes = {}
+Footer.propTypes = {};
 
-export default Footer
+export default Footer;
