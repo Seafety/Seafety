a2a601c53db5483598e2a35249aa553cce0cecc0
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-06-05 23:30:37 -0300

    feat: :sparkles: applying grid and adding header body
    applying grid and adding header body

--- src/ui/Components/Header.jsx
+++ src/ui/Components/Header.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Botao from "./Botao";
 
-const Header = props => {
+import SeafetyLogo from "../../Assets/Seafety.svg";
+
+const Header = (props) => {
+  const navigate = useNavigate();
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <img src={SeafetyLogo} alt="" srcset="" className="img-seafety" />
 
-Header.propTypes = {}
+      <nav>
+        <ul className="nav-ul">
+          <li onClick={() => navigate("/")}>Demo</li>
+          <li onClick={() => navigate("/Meio-Ambiente")}>Meio Ambiente</li>
+          <li onClick={() => navigate("/TubAI")}>TubAI</li>
+          <li onClick={() => navigate("/Documentacao")}>Documentação</li>
+          <li onClick={() => navigate("/")}>Contato</li>
+          <li onClick={() => navigate("/Demo")}>
+            <Botao>Entrar</Botao>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+Header.propTypes = {};
+
+export default Header;
