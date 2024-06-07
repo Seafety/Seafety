772521ebc1353108d3a7f77a741f96b31818287e
Lucy <luboutti19@gmail.com>
2024-06-07 14:46:40 -0300

    😎 clean code - responsive layout

--- src/Pages/TubAI/TubAI.jsx
+++ src/Pages/TubAI/TubAI.jsx
@@ -12,6 +12,7 @@ import core from "../../Assets/TubaiAssets/heart.png"
 import book from "../../Assets/TubaiAssets/book.png"
 import cloud from "../../Assets/TubaiAssets/cloud.png"
 import medalha from "../../Assets/TubaiAssets/badge.png"
+import { Link } from "react-router-dom";
 
 const TubAI = props => {
   return (
@@ -20,42 +21,36 @@ const TubAI = props => {
       {/* BANNER */}
 
       <div className='bannertubai'>
+        <img src={banner} />
+        </div>
    
         <div className='txtbanner1'>
           <div className='txts'>
-          <div className='txt1'>
-            <h1>57 mil</h1>
-            <p>Litros de combustível economizados</p>
-          </div>
+            <div className='txt1'>
+              <h1>57 mil</h1>
+              <p>Litros de combustível economizados</p>
+            </div>
 
-          <div className='txt2'>
+          <div className='txt1'>
             <h1>90TB</h1>
             <p>de dados analisados
               pela nossa inteligência.</p>
           </div></div>
 
-          <div className='txt3'>
+          <div className='txt1'>
             <h1>67%</h1>
             <p>a mais de
               segurança marítimica.</p>
           </div>
 
-          <button className='btnbanner'>Explore</button>
+          <button className='blue-button' style={{margin: '4%'}}>Explore</button>
         </div>
-
-       
-
+    
         <div className='txtbanner2'>
           <p>Nós somos a <b>Seafety</b>.</p>
           <p className='pblue'>o oceano.</p>
-          <p className='pbluefade'>o mundo.</p>
-
-        </div> 
-        
-      </div>
-      
-      {/* BOX1 */}
-
+          <p className='pbluefade'>o mundo.</p>       
+        </div>
 
       <div className='pbox1'>
         <p>O melhor jeito de utilizar a inteligência artificial.
@@ -64,27 +59,31 @@ const TubAI = props => {
       
       <div className='links'> 
           <div className='box1'>
-            <h3>Rotas <b className='pblue'>Otimizadas</b>.</h3>
-            <img className='imgbox1' src={box1} />
-            <button className='btnlinks'><p className='pbtn'>Rotas</p></button>
+            <div className='insidebox'>
+              <h3>Rotas <b className='pblue'>Otimizadas</b>.</h3>
+              <img className='imgbox1' src={box1} />
+              <button className='btnlinks'><p className='pbtn'>Rotas</p></button>
+            </div>
           </div>
         
-        <div className='box2'>
-          <h3>Analise <b className='pblue'>Oceânicas</b>.</h3>
-          <img className='imgbox2' src={box2} />    
-          <button className='btnlinks'><p className='pbtn'>Dashboard</p></button>
+        <div className='box1'>
+          <div className='insidebox'>
+            <h3>Analise <b className='pblue'>Oceânicas</b>.</h3>
+            <img className='imgbox1' src={box2} />    
+            <button className='btnlinks'><p className='pbtn'>Dashboard</p></button>
+          </div>
         </div>
 
-        <div className='box3'>
-          <h3>Oceano <b className='pblue'>Seguro</b>.</h3>
-          <img className='imgbox3' src={box3} />
-          <button className='btnlinks'><p className='pbtn'>Seafety</p></button>
+        <div className='box1'>
+          <div className='insidebox'>
+            <h3>Oceano <b className='pblue'>Seguro</b>.</h3>
+            <img className='imgbox1' src={box3} />
+            <button className='btnlinks'><p>Seafety</p></button>
+          </div>
         </div>
       </div>
 
-      
-      {/* BOX2 */}
-      <div className='boxao2'>
+      <div className='icons-container'>
       <div className='box2div1'>
         <h1>Como a <b className='pblue'>TubAI</b> pode</h1> 
         <h1>te ajudar hoje?</h1>
@@ -104,48 +103,38 @@ const TubAI = props => {
             <h4>Formar Padrões</h4>
             <p>Identificamos padrões na natureza oceanica.</p>
           </div> 
-            
+           
         </div>
-
-          <div className='icon3'>
-          
+          <div className='icon'>
             <img src={icon3}  />
             <h4 >Análise Preventiva</h4>
             <p>Tendências para a melhor tomada de decisões.</p>
         </div>
       </div></div>
 
-      {/* BOX3 */}
-      <h1 className='h1box3'>Insights e Previsões de Sucesso</h1>
-      <div className='boxao3' >
-        <div className='box1box3'>
+      <div className='titleinsight'><h1>Insights e Previsões de Sucesso</h1></div>
+      <div className='insights'>
+        <div className='prevision'>
           <div className='titulo1'>
-            <div><img src={exclamacao} /></div>
-               
-            <div ><b >Previsão de Tempestade Costeira</b></div>
+            <img src={exclamacao} />            
+            <div ><b>Previsão de Tempestade Costeira</b></div>
           </div>
           <p>A IA da Seafety analisou dados meteorológicos e marítimos, indicando a formação de uma tempestade severa. Com base em padrões históricos e condições.</p>
           <p><b>Risco:</b> Áreas Vulneráveis</p>
         </div>
 
-        <div className='box2box3'>
+        <div className='preservations'>
           <div className='turtle'>
             <div className='titulo1'>
-            <div><img src={core} /></div>
+            <img src={core} />
                
             <div><b>Preservação Ambiental: Tartatuguinhas!</b></div>
             </div>
             <p>A época de desova é regida principalmente pela temperatura, ocorrendo nos períodos mais quentes do ano. Pela análise da nossa IA, prevemos localizações e datas.</p>
           </div>
-
-          <div>
             <button className='btnbbox3'>Veja mais previsões</button>
-          </div>
         </div>
       </div>
-
-      {/* PLANOS */}
-
 
       <div className="plans-container">
       <h1 className="plans-title">Planos</h1>
@@ -228,23 +217,15 @@ const TubAI = props => {
             <td></td>
             <td>&#10003;</td>
           </tr>
+          <tr>
+            <td style={{border:'none'}}></td>
+            <td style={{border:'none'}}><button className="price-button">Gratuito</button></td>
+            <td style={{border:'none'}}><button className="price-button">$64/mês</button></td>
+            <td style={{border:'none'}}><button className="price-button">$100/mês</button></td>
+          </tr>
         </tbody>
       </table>
-      <div className="pricing">
-        <div className="price-option">
-          <button className="price-button">Gratuito</button>
-        </div>
-        <div className="price-option">
-          <button className="price-button">$64/mês</button>
-        </div>
-        <div className="price-option">
-          <button className="price-button">$100/mês</button>
-        </div>
-      </div>
     </div>
-
-
-
      </div>
   )
 }
