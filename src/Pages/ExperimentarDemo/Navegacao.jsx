1cb071c6597ffffd26b7e19ec8fa44d10c1c264f
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-06-07 19:09:13 -0300

    feat: :sparkles: adding navigation screen
    adding navigation screen

--- src/Pages/ExperimentarDemo/Navegacao.jsx
+++ src/Pages/ExperimentarDemo/Navegacao.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import WorldMap from "../../ui/Components/WorldMap";
+import red_exclamation from "../../Assets/Demo/red-exclamation.png";
+import green_time from "../../Assets/Demo/green-time-icon.png";
 
 const Navegacao = () => {
   return (
-    <div>Navegacao</div>
-  )
-}
+    <body className="navegacao-body">
+      <h3 className="welcome">Bem vindo(a) a bordo.</h3>
+      <h1 className="ship-title">Navio BR01FR</h1>
+      <section className="info-section">
+        <WorldMap />
+        <div className="info-container">
+          <div className="details">
+            <p>
+              <strong>Percurso:</strong> 9.708,13 km
+            </p>
+            <p>
+              <strong>Maior onda registrada:</strong> 1.5m
+            </p>
+            <p>
+              <strong>Temperatura da Superfície Oceânica média:</strong> 27°C
+            </p>
+            <p>
+              <strong>Tempo de Percurso:</strong> 15 dias
+            </p>
+          </div>
+          <ul className="menu">
+            <li>CARGA</li>
+            <li>MELHOR ROTA</li>
+            <li>TEMPO BOM</li>
+          </ul>
+          <div className="warnings">
+            <h4>Avisos:</h4>
+            <div className="warning">
+              <img src={red_exclamation} alt="" />
+              <div className="warning-flex">
+                <strong>Tempestade prevista de 4.000,00km à 5.000,00km.</strong>
+                <p>Vento, Direção: 4.0m/s, 80.0°</p>
+              </div>
+            </div>
+            <div className="warning">
+              <img src={green_time} alt="" />
+              <div className="warning-flex">
+                <strong>Tempestade prevista de 4.000,00km à 5.000,00km.</strong>
+                <p>Vento, Direção: 4.0m/s, 80.0°</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
+  );
+};
 
-export default Navegacao
+export default Navegacao;
