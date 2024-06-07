dd56573805b556ebcf4e26ec857ad76ba2332cd9
Lucy <luboutti19@gmail.com>
2024-06-06 22:45:16 -0300

    commit base banner

--- src/Pages/Documentacao/Documentacao.jsx
+++ src/Pages/Documentacao/Documentacao.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import ana from "../../Assets/PageAssets/ana.png"
+import lucy from "../../Assets/PageAssets/lucy.png"
+import clara from "../../Assets/PageAssets/clara.png"
+import thiago from "../../Assets/PageAssets/thiago.png"
+import miguel from "../../Assets/PageAssets/miguel.png"
+import docBanner from "../../Assets/PageAssets/bannerDoc.jpg"
+
+
 const Documentacao = props => {
   return (
-    <div>Documentacao</div>
+    <div className='documentacao'>      
+    <div className='banner-container'>
+      <img className='banner' src={docBanner} />
+      <div className='docbutton'>
+      <h1>Confira a nossa documentação</h1> 
+      <button className='blue-button'>Escopo</button>
+    </div>
+  </div>
+
+
+    </div>
   )
 }
 
