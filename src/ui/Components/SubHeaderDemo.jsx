0117043c0bde107d79ce4b1a17722686ef54b6cc
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2024-06-07 02:22:36 -0300

    feat: :sparkles: adding subHeader from demo
    adding subHeader from demo

--- src/ui/Components/SubHeaderDemo.jsx
+++ src/ui/Components/SubHeaderDemo.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const SubHeaderDemo = () => {
+  const navigate = useNavigate();
+  const [activeLink, setActiveLink] = useState("/Demo/Dashboard");
+  useEffect(() => {
+    setActiveLink("/Demo/Dashboard");
+  }, [])
   return (
-    <div>SubHeaderDemo</div>
+      <nav className='nav-subheader'>
+        <ul className="nav-subheader-ul">
+          <li
+            className={activeLink === "/Demo/Dashboard" ? "active" : ""}
+            onClick={() => {
+              navigate("/Demo/Dashboard");
+              setActiveLink("/Demo/Dashboard");
+            }}
+          >
+            Dashboard
+          </li>
+          <li
+            className={activeLink === "/Demo/Navegacao" ? "active" : ""}
+            onClick={() => {
+              navigate("/Demo/Navegacao");
+              setActiveLink("/Demo/Navegacao");
+            }}
+          >
+            Navegação
+          </li>
+        </ul>
+      </nav>
   )
 }
 
